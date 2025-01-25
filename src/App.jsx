e1c9ad2ac49fcf09bf30e23d046813c3a1315c29
Veer <veerksheth@gmail.com>
2025-01-25 12:59:48 +0000

    add lenis based scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import "./App.scss";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import TitlePage from "./components/TitlePage";
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 
 function App() {
+  const lenis = useLenis(({ scroll }) => {});
+
   return (
+    <ReactLenis root>
     <div id="app">
       <TitlePage />
       <About />
       <Projects />
     </div>
- );
+    </ReactLenis>
+  );
 }
 
 export default App;
